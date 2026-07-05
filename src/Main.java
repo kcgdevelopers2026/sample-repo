ceeac48dd27dc8810052aedaba0738d0ae7744c2
KCGDEVELOPER_MZH <kcgdevelopers@gmail.com>
2026-07-05 19:24:24 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ class Main{
     public static void  main(String[] args){
 
         System.out.print("Hello world");
-        System.out.print("zageee");
+        System.out.print("zageeeascc");
     }
 }
